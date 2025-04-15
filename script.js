4b62c7707e793a69d9ff8df19a6d63e4d107e664
UrbanGoCabs <urbangocabs@gmail.com>
2025-04-15 22:50:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+<script>
+  const form = document.getElementById('bookingForm');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: data
+    }).then(res => {
+      if (res.ok) {
+        alert("Booking Successful!");
+        form.reset();
+      } else {
+        alert("Booking Failed. Please try again.");
+      }
+    });
+  });
+</script>
